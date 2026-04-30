020aee59530265735e32d7f3958e7c65d5f8d1fa
kbw19 <kwagholikar@mgh.harvard.edu>
2026-04-30 16:16:16 -0400

    get level

--- src/tabshell/ont.py
+++ src/tabshell/ont.py
@@ -4,3 +4,6 @@ class Ont():
 
     def get_concepts(self,query_path):
         return [x.split('/')[-1] for x in self.path_list if query_path in x]
+    
+    def get_level(self,level=1):
+        return list(set(['/'.join(x.split('/')[0:level+1]) for x in self.path_list]))
